c648605efb61e71de1604685083a7f78bd476285
Rajagopal <rajagopalnaidu.m@gmail.com>
2024-08-26 15:56:15 -0700

    STring & Slice

--- Prereq.py
+++
@@ -1,1 +0,0 @@
-No prereq for this time
---
+++ Testtest.py
@@ -0,0 +1,10 @@
+city=input("Enter your city name: ").lower().strip()
+print(city)
+if city == 'hyderabad':
+    print("Hellow Hyderabad...Good Morning!!!")
+elif city == 'chennai':
+    print("Hellow Chennai...Good Morning!!!")
+elif city == "delhi":
+    print("Hellow Delhi...Good Morning!!!")
+else:
+    print("Given input is not in range")
